3b7afe873c40d3f5d95b0ed2599ea424be5f6f04
Guillermo Williamson <gwilliamtam@gmail.com>
2018-02-19 17:40:33 -0600

    Add practice form

--- resources/views/practice.blade.php
+++ resources/views/practice.blade.php
@@ -20,170 +20,165 @@
 
             <div class="row">
                 @foreach($practices as $practice)
-                <div class="col-md-4">
-                    <div class="card mb-4 box-shadow card-practice-container">
+                    <div class="col-md-4">
+                        <div class="card mb-4 box-shadow card-practice-container">
 
-                        <div class="card-practice-images">
-                            @if($practice->location)
-                                <div class="practice-element" data-img="/loc_images/{{ $practice->location->photo }}">
-                                    <img src="/loc_images/{{ $practice->location->photo }}">
-                                </div>
-                            @endif
-                            @if($practice->gear)
-                                <div class="practice-element" data-img="/gear_images/{{ $practice->gear->photo }}">
-                                    <img src="/gear_images/{{ $practice->gear->photo }}">
-                                </div>
-                            @endif
-                            @if($practice->ammo)
-                                <div class="practice-element" data-img="/ammo_images/{{ $practice->ammo->photo }}">
-                                    <img src="/ammo_images/{{ $practice->ammo->photo }}">
-                                </div>
-                            @endif
-
-                            @foreach($practice->targets as $target)
-                                @if(empty($target->photo))
-                                    <div class="practice-element">
-                                @else
-                                    <div class="practice-element" style="background-image: url('/target_images/{{ $target->photo }}');">
-                                @endif
-                                @if(!empty($target->value))
-                                    <div class="target-value">
-                                        {{ $target->value }}
+                            <div class="card-practice-images">
+                                @if($practice->location)
+                                    <div class="practice-element" data-img="/loc_images/{{ $practice->location->photo }}">
+                                        <img src="/loc_images/{{ $practice->location->photo }}">
                                     </div>
                                 @endif
+                                @if($practice->gear)
+                                    <div class="practice-element" data-img="/gear_images/{{ $practice->gear->photo }}">
+                                        <img src="/gear_images/{{ $practice->gear->photo }}">
                                     </div>
-                            @endforeach
-                            {{--@for($i=1;$i<=20;$i++)--}}
-                                    {{--<div class="practice-element">--}}
-                                        {{--<img src="/images/targetTest.jpg">--}}
-                                    {{--</div>--}}
-                            {{--@endfor--}}
-                        </div>
+                                @endif
+                                @if($practice->ammo)
+                                    <div class="practice-element" data-img="/ammo_images/{{ $practice->ammo->photo }}">
+                                        <img src="/ammo_images/{{ $practice->ammo->photo }}">
+                                    </div>
+                                @endif
+
+                                @foreach($practice->targets as $target)
+                                    <div class="practice-element target-element" style="background-image: url('/target_images/{{ $target->photo }}');" data-img="/target_images/{{ $target->photo }}" data-value="{{ $target->value }}">
+                                        @if(!empty($target->value))
+                                            <div class="target-value">
+                                                {{ $target->value }}
+                                            </div>
+                                        @endif
+                                    </div>
+                                @endforeach
+                            </div>
 
 
-                        <div class="card-body">
-                            <h4 class="card-text">{{ $practice->date_time }}</h4>
-                            <div class="d-flex justify-content-between align-items-center">
-                                <div class="btn-group" data-id="{{ $practice->id }}" data-json="{{ json_encode($practice) }}">
-                                    <button type="button" class="btn btn btn-dark target-action">
-                                        <i class="fas fa-bullseye"></i>
-                                    </button>
-                                    <button type="button" class="btn btn btn-dark add-action">
-                                        <i class="fas fa-plus"></i>
-                                    </button>
-                                    <button type="button" class="btn btn btn-dark edit-action">
-                                        <i class="fas fa-edit"></i>
-                                    </button>
-                                    <button type="button" class="btn btn btn-dark del-action" >
-                                        <i class="fas fa-times"></i>
-                                    </button>
+                            <div class="card-body">
+                                <h4 class="card-text">{{ $practice->date_time }}</h4>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <div class="btn-group" data-id="{{ $practice->id }}" data-json="{{ json_encode($practice) }}">
+                                        <button type="button" class="btn btn btn-dark target-action">
+                                            <i class="fas fa-bullseye"></i>
+                                        </button>
+                                        <button type="button" class="btn btn btn-dark add-action">
+                                            <i class="fas fa-plus"></i>
+                                        </button>
+                                        <button type="button" class="btn btn btn-dark edit-action">
+                                            <i class="fas fa-edit"></i>
+                                        </button>
+                                        <button type="button" class="btn btn btn-dark del-action" >
+                                            <i class="fas fa-times"></i>
+                                        </button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
                 @endforeach
             </div>
-        </div>
-    </div>
 
-    {{-- FORM TO ADD TARGETS --}}
-    <div class="modal" tabindex="-1" role="dialog" id="target-modal">
-        <form action="{{ route('practice.addTarget') }}" method="post" enctype="multipart/form-data" id="target-form">
-            <input type="hidden" name="practiceId" value="">
-            <input type="hidden" value="{{ csrf_token() }}" name="_token">
+            {{-- FORM TO ADD PRACTICE --}}
+            <div class="modal" tabindex="-1" role="dialog" id="add-practice-modal">
+                <form action="{{ route('practice.save') }}" method="post" id="practice-form">
+                    <input type="hidden" name="practiceId" value="">
+                    <input type="hidden" value="{{ csrf_token() }}" name="_token">
 
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title">Add Target</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title">Add Practice</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
 
-                        {{-- Target Value --}}
-                        <div class="form-group">
-                            <input type="text" class="form-control" name="value" id="value" aria-describedby="value" placeholder="Enter target value">
-                            <small id="valueHelp" class="form-text text-muted">You may enter the target value(optional)</small>
+                                @include("partials.date-picker", [
+                                    "dateField" => "date-time"
+                                ])
+
+                                {{-- Practice Date Time --}}
+                                <div class="form-group">
+                                    <input type="text" class="form-control" name="date_time" id="date-time" aria-describedby="dateTimeHelp" placeholder="Enter date and time" value="{{ date("l, Y-m-d H:i") }}">
+                                    <small id="dateTimeHelp" class="form-text text-muted">You must enter date and time of practice (require)</small>
+                                </div>
+
+                            </div>
+                            <div class="modal-footer">
+                                <button type="submit" class="btn btn-dark">Save</button>
+                                <button type="button" class="btn btn-outline-dark" data-dismiss="modal">Cancel</button>
+                            </div>
                         </div>
-
-                        {{-- Photo Upload --}}
-                        <div class="form-group">
-                            <label for="photo" class="btn btn-outline-dark">Select Photo</label>
-                            <input type="file" accept="image/*" style="visibility:hidden;" name="photo" id="photo">
-                            <img id="photo-preview">
-                            <small id="photoHelp" class="form-text text-muted">Add a photo of your target(optional)</small>
-                        </div>
-
                     </div>
-                    <div class="modal-footer">
-                        <button type="submit" class="btn btn-dark">Save</button>
-                        <button type="button" class="btn btn-outline-dark" data-dismiss="modal">Cancel</button>
-                    </div>
-                </div>
+                </form>
             </div>
-        </form>
-    </div>
 
-        {{-- FORM TO ADD TARGETS --}}
-        <div class="modal" tabindex="-1" role="dialog" id="target-modal">
-            <form action="{{ route('practice.addTarget') }}" method="post" enctype="multipart/form-data" id="target-form">
-                <input type="hidden" name="practiceId" value="">
-                <input type="hidden" value="{{ csrf_token() }}" name="_token">
+            {{-- FORM TO ADD TARGETS --}}
+            <div class="modal" tabindex="-1" role="dialog" id="target-modal">
+                <form action="{{ route('practice.addTarget') }}" method="post" enctype="multipart/form-data" id="target-form">
+                    <input type="hidden" name="practiceId" value="">
+                    <input type="hidden" value="{{ csrf_token() }}" name="_token">
 
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title">Add Target</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+
+                                {{-- Target Value --}}
+                                <div class="form-group">
+                                    <input type="text" class="form-control" name="value" id="value" aria-describedby="value" placeholder="Enter target value">
+                                    <small id="valueHelp" class="form-text text-muted">You may enter the target value(optional)</small>
+                                </div>
+
+                                {{-- Photo Upload --}}
+                                <div class="form-group">
+                                    <label for="photo" class="btn btn-outline-dark">Select Photo</label>
+                                    <input type="file" accept="image/*" style="visibility:hidden;" name="photo" id="photo">
+                                    <img id="photo-preview">
+                                    <small id="photoHelp" class="form-text text-muted">Add a photo of your target(optional)</small>
+                                </div>
+
+                            </div>
+                            <div class="modal-footer">
+                                <button type="submit" class="btn btn-dark">Save</button>
+                                <button type="button" class="btn btn-outline-dark" data-dismiss="modal">Cancel</button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+
+            <div class="modal" tabindex="-1" role="dialog" id="view-modal">
                 <div class="modal-dialog" role="document">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h5 class="modal-title">Add Target</h5>
+                            <h5 class="modal-title"></h5>
                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                 <span aria-hidden="true">&times;</span>
                             </button>
                         </div>
-                        <div class="modal-body">
-
+                        <div class="modal-body view-modal-img-container">
+                            <img id="view-target-image">
                             {{-- Target Value --}}
-                            <div class="form-group">
-                                <input type="text" class="form-control" name="value" id="value" aria-describedby="value" placeholder="Enter target value">
+                            <div class="form-group" id="edit-target-value">
+                                <input type="text" class="form-control" name="value" id="targetValue" aria-describedby="value" placeholder="Enter target value">
                                 <small id="valueHelp" class="form-text text-muted">You may enter the target value(optional)</small>
                             </div>
-
-                            {{-- Photo Upload --}}
-                            <div class="form-group">
-                                <label for="photo" class="btn btn-outline-dark">Select Photo</label>
-                                <input type="file" accept="image/*" style="visibility:hidden;" name="photo" id="photo">
-                                <img id="photo-preview">
-                                <small id="photoHelp" class="form-text text-muted">Add a photo of your target(optional)</small>
-                            </div>
-
                         </div>
                         <div class="modal-footer">
                             <button type="submit" class="btn btn-dark">Save</button>
                             <button type="button" class="btn btn-outline-dark" data-dismiss="modal">Cancel</button>
                         </div>
                     </div>
-                </div>
-            </form>
-        </div>
-    </div>
-
-
-    <div class="modal" tabindex="-1" role="dialog" id="view-modal">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title"></h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-
+                    </form>
                 </div>
             </div>
+
         </div>
-        </form>
     </div>
 
     <script>
@@ -241,8 +236,28 @@
 
             $(".practice-element").on("click", function(){
                 var image = this.attributes['data-img'].value
-                console.log(image)
-                $("#view-modal .modal-body").html("<img class='view-model-img' src='" + image + "'>")
+                $("#view-target-image").attr("src", image)
+                $("#value").hide()
+                $("#view-modal").modal("show")
+            })
+
+            $(".target-element").on("click", function(){
+                var image = this.attributes['data-img'].value
+                var targetValue = this.attributes['data-value'].value
+                console.log(image, targetValue, image.length, targetValue.length)
+                if(image.length>0){
+                    $("#view-target-image").attr("src", image)
+                    $("#view-target-image").show()
+                }else{
+                    $("#view-target-image").hide()
+                }
+
+                if(targetValue.length>0){
+                    $("#targetValue").val(targetValue)
+                    $("#edit-target-value").show()
+                }else{
+                    $("#edit-target-value").hide()
+                }
                 $("#view-modal").modal("show")
             })
         })
